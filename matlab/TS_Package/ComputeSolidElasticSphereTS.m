31af159b44bd76286d3d7c8c0c5a88e38df42475
Gavin Macaulay <gavin@macaulay.co.nz>
2011-11-27 10:55:13 +0100

    Make the code work when not in the directory with the starting code Update the instructions to include installation directions

--- matlab/TS_Package/ComputeSolidElasticSphereTS.m
+++ matlab/TS_Package/ComputeSolidElasticSphereTS.m
@@ -2,9 +2,17 @@
 % The TS package is a Graphic User Interface (GUI) based MATLAB software  
 % to compute the Target Strength  (TS) of a solid elastic sphere. 
 
-clear all
-close all
-addpath  functions functions\generic sw_property
+%clear all
+%close all
+fullname = mfilename('fullpath');
+[p, name, ext] = fileparts(fullname);
+
+% Add to path, but don't save so as to not disturb the user's permament
+% paths. This adds to the top of the path so will superceed any function on
+% the existing path that may have the same name.
+addpath(fullfile(p, 'aux_functions'), ...
+    fullfile(p, 'aux_functions\generic'), ...
+    fullfile(p, 'sw_property'));
 
 global out para
 
