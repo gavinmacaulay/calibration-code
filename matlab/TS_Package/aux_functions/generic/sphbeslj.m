4ed6b2d5445dc9b26fe1e6b3740905e403e851c4
Gavin Macaulay <gavin@macaulay.co.nz>
2012-05-15 16:07:25 +0200

    Remove a "won't work in a future version of Matlab version" warning

--- matlab/TS_Package/aux_functions/generic/sphbeslj.m
+++ matlab/TS_Package/aux_functions/generic/sphbeslj.m
@@ -6,7 +6,10 @@ function 	jn=sphbeslj(n,x)
 indx=find(abs(x) < 1e-14);
 x(indx)=1e-10*ones(size(indx));
 x=x(:);
-Jm_1_2=besselj(n+1/2,x);
+%Jm_1_2=besselj(n+1/2,x); % this form will not work in future versions of
+                          % Matlab.
+[NU1 Z1] = meshgrid(n+1/2,x);
+Jm_1_2 = besselj(NU1, Z1);
 if (length(n) == 1)
   jn=sqrt(pi./(2*x)).*Jm_1_2;
   return
