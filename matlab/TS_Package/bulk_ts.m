c1a5b6bc681ba9cdbc4fcc7f2c3a6e5ca850ec5d
Gavin Macaulay <gavin@macaulay.co.nz>
2012-05-16 09:04:15 +0200

    Bulk calculations now work over multiple averaging bandwidths

--- matlab/TS_Package/bulk_ts.m
+++ matlab/TS_Package/bulk_ts.m
@@ -1,7 +1,26 @@
 %%
 % Ad-hoc code to produce bulk sphere TS estimates. Just alter the section
-% surrounded by %%%%%%% and for a different sphere material, the values in the 
-% para struct
+% surrounded by %%%%%%%.
+% Will generate a table for all combinations of the given sound speed and
+% averaging bandwidth.
+
+%%%%%%%
+cw = 1450:5:1520; % water sound speed [m/s]
+rhow = 1025.3288; % water density [kg/m^3]
+% WC
+rhos = 14947; % density of sphere [m/s]
+cc = 6853; % compressional sound speed in sphere [m/s]
+cs = 4171; % shear sound speed in sphere [m/s]
+% Cu
+%rhos = 8947: % density of sphere [m/s]
+%cc = 4760; % compressional sound speed in sphere [m/s]
+%cs = 2288.5; % shear sound speed in sphere [m/s]
+
+freq_spec = 38; % TS at this freq [kHz]
+ave_BW = [3.675 3.275 2.425 1.448 0.766]; % bandwidths to average over [kHz]
+D = 38.1; % sphere diameter [mm]
+%%%%%%%
+
 freq_range = [0.01 473.7720];
 scale = 1;
 n = 4000;
@@ -11,24 +30,20 @@ proc_flag = 1;
 T = [];
 P = [];
 S = [];
-cw = [1450:5:1520]; % water sound speed [m/s]
-rhow = 1025.3288; % water density [kg/m^3]
 
-%%%%%%%
-freq_spec = 333; % TS at this freq [kHz]
-ave_BW = 27.944; % bandwidth to average over [kHz]
-D = 10; % sphere diameter [mm]
-%%%%%%%
 
-disp(['For c = ' num2str(cw) ' m/s'])
+disp(['c\bw ' num2str(ave_BW, '%.3f ')])
 for i = 1:length(cw)
-    % for WC
-    para = struct('rho', 14900, 'cc', 6853, 'cs', 4171, 'ave_value', ave_BW, ...
-        'ave_unit', 0, 'n', n, 'out_flag', 2, 'a', D/2/1000, 'cw', cw(i), ...
-        'rhow', rhow, 'freq_range', freq_range, 'freq_spec', freq_spec, ...
-        'ave_BW', ave_BW, 'scale', scale, 'target_index', target_index, ...
-        'proc_flag', proc_flag, 'D', D, 'T', T, 'P', P, 'S', S);
+    r = [];
+    for j = 1:length(ave_BW)
+        para = struct('rho', rhos, 'cc', cc, 'cs', cs, 'ave_value', ave_BW(j), ...
+            'ave_unit', 0, 'n', n, 'out_flag', 2, 'a', D/2/1000, 'cw', cw(i), ...
+            'rhow', rhow, 'freq_range', freq_range, 'freq_spec', freq_spec, ...
+            'ave_BW', ave_BW(j), 'scale', scale, 'target_index', target_index, ...
+            'proc_flag', proc_flag, 'D', D, 'T', T, 'P', P, 'S', S);
 
-    [para,out]=solid_elastic_sphere_TS_fun(freq_range,freq_spec,scale,n,target_index,proc_flag,D,T,P,S,cw(i),rhow,ave_BW,para);
-    disp([num2str(out.TS_spec_ave,'%.2f')])
+        [para,out]=solid_elastic_sphere_TS_fun(freq_range,freq_spec,scale,n,target_index,proc_flag,D,T,P,S,cw(i),rhow,ave_BW,para);
+        r = [r out.TS_spec_ave];
+    end
+    disp([num2str(cw(i)) ' ' num2str(r,'%.2f ')])
 end
