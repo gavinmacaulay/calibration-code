e6bf82ac1a1c00e5ba09461d1f0be9183b024ab8
Gavin Macaulay <gavin@macaulay.co.nz>
2012-05-31 13:10:00 +0200

    Enable auto recalculation of ytick labels when zooming the TS/freq plotyy Also fix a brace warning

--- matlab/TS_Package/aux_functions/pushbuttonComp_Callback.m
+++ matlab/TS_Package/aux_functions/pushbuttonComp_Callback.m
@@ -213,12 +213,13 @@ if para.proc_flag == 1
     xlabel('FREQUENCY (kHz)','fontweight','bold','fontsize',14)
     set(get(ax(1), 'YLabel'), 'String', 'TARGET STRENGTH (dB)','fontweight','bold','fontsize',14)
     set(get(ax(2), 'YLabel'), 'String', 'TARGET PHASE (\circ)','fontweight','bold','fontsize',14)
-    legend(['Theory (no ave)'],['Theory (with ave)'], 4)
+    legend('Theory (no ave)', 'Theory (with ave)', 4)
 %    text(freq0-2,out.TS_spec-2,freq_str,'color','r');
     title([out.t_str ' (' sprintf('%4.3g mm',D) ')'],'fontweight','bold','fontsize',14);
     axis(ax(1), [freq_range min(-80,min(out.TS)-5) max(-30,max(out.TS)+5)])
-    %axis(ax(2), [freq_range min(-80,min(out.TS)-5) max(-30,max(out.TS)+5)])
     xlim(ax(2), freq_range)
+    set(ax(1), 'YTickMode', 'auto') % plotyy turns this off, which causes problems when zooming
+    set(ax(2), 'YTickMode', 'auto')
     fprintf('\tfreq (kHz)\t\t TS\t\t TS (ave)\n')
     for i=1:length(freq_spec)
         fprintf('\t\t%d\t\t %8.3f\t %8.3f\n',para.freq_spec(i),out.TS_spec(i),out.TS_spec_ave(i));
