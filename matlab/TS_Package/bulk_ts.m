92f708784265763ca44e278cf46d65ca33da08f6
Gavin Macaulay <gavin@macaulay.co.nz>
2013-05-27 08:22:52 +0200

    adjusted WC density and sound speeds to match those from a paper Included the paper that the sphere material properties come from Added in the bandwidths for the 333 kHz transducer

--- matlab/TS_Package/bulk_ts.m
+++ matlab/TS_Package/bulk_ts.m
@@ -8,22 +8,27 @@
 cw = 1450:5:1520; % water sound speed [m/s]
 rhow = 1025.3288; % water density [kg/m^3]
 %rhow = 1027
+
 % WC
-rhos = 14947; % density of sphere [m/s]
-cc = 6853; % compressional sound speed in sphere [m/s]
-cs = 4171; % shear sound speed in sphere [m/s]
+rhos = 14900; % density of sphere [m/s], from [1]
+cc = 6864; % compressional sound speed in sphere [m/s], from [1]
+cs = 4161.2; % shear sound speed in sphere [m/s], from [1]
 % Cu
-%rhos = 8947: % density of sphere [m/s]
-%cc = 4760; % compressional sound speed in sphere [m/s]
-%cs = 2288.5; % shear sound speed in sphere [m/s]
+%rhos = 8947: % density of sphere [m/s], from [1]
+%cc = 4760; % compressional sound speed in sphere [m/s], from [1]
+%cs = 2288.5; % shear sound speed in sphere [m/s], from [1]
+
+% References
+%  [1] Foote, K. G., and MacLennan, D. N. 1984. Comparison of copper and tungsten carbide calibration spheres. The Journal of the %        Acoustical Society of America, 75: 612–616.
+
 
 % % For Simrad EK60. Bandwidths to average over [kHz]
 %spec = struct('freq', 18, 'BW', [1.73 1.56 1.17 0.71 0.38]); % for 18 kHz pulse lengths (512, 1024, 2048, 4096, 8192)
 %spec = struct('freq', 38, 'BW', [3.675 3.275 2.425 1.448 0.766]); % for 38 kHz pulse lengths (256, 512, 1024, 2048, 4096)
 %spec = struct('freq', 70, 'BW', [6.74 6.09 4.63 2.83 1.51]); % for 70 kHz pulse lengths (128, 256, 512, 1024, 2048)
 spec = struct('freq', 120, 'BW', [11.66 10.79 8.61 5.49 2.99]); % for 120 kHz pulse lengths (64, 128, 256, 512, 1024)
-%spec = struct('freq', 200, 'BW', 18.54 15.55 10.51 5.90 3.05]); % for 200 kHz pulse lengths (64 128 256 512 1024)
-%spec = struct('freq', 333, 'BW', [64 128 256 512 1024]); % for 333 kHz pulse lengths (64 128 256 512 1024)
+%spec = struct('freq', 200, 'BW', [18.54 15.55 10.51 5.90 3.05]); % for 200 kHz pulse lengths (64 128 256 512 1024)
+%spec = struct('freq', 333, 'BW', [27.944 20.078 11.720 6.145 3.112]); % for 333 kHz pulse lengths (64 128 256 512 1024)
 
 D = 22; % sphere diameter [mm]
 %%%%%%%
